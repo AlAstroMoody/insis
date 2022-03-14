56d871642700d594ba4bc640e786e0801caaffe9
alex <aastrotenko@gmail.com>
2022-03-14 13:04:41 +0700

    add table for unit page

--- src/components/AppUnit.vue
+++ src/components/AppUnit.vue
@@ -4,14 +4,41 @@
       :value="searchValue"
       @changeSearchValue="changeSearchValue($event)"
     />
-    {{ pageData }}
+
+    <el-table :data="pageData" style="width: 100%" class="unit__table table">
+      <el-table-column prop="data.code" label="Код подразделения" width="180" />
+      <el-table-column prop="data.name" label="Название" />
+      <el-table-column
+        prop="data.region_code"
+        label="Код региона"
+        width="150"
+      />
+      <el-table-column label="Вид подразделения" width="180">
+        {{ unitType }}
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      layout="prev, pager, next"
+      :total="data.length"
+      :hide-on-single-page="true"
+      :page-size="pageSize"
+      :current-page="currentPage + 1"
+      @current-change="currentPageChange"
+      prev-text="В начало"
+      next-text="В конец"
+      background
+      class="table"
+    />
   </section>
 </template>
 
 <script setup>
+import { ElTable, ElPagination } from 'element-plus'
 import { computed, ref, toRefs } from 'vue'
 
 import AppSearch from '@/components/AppSearch.vue'
+import { units } from '~/constants.js'
 
 const props = defineProps({
   data: {
@@ -24,11 +51,27 @@ const props = defineProps({
   },
 })
 const { data } = toRefs(props)
-const page = ref(0)
-const lineInPage = 5
+const currentPage = ref(0)
+const pageSize = 5
+
+const unitType = computed(() => {
+  for (const unit of units) {
+    if (
+      data.value.length &&
+      unit.type === units[data.value[0].data.type].type
+    ) {
+      return unit.title
+    }
+  }
+
+  return ''
+})
 
 const pageData = computed(() =>
-  data.value.slice(page.value * lineInPage, page.value + 1 * lineInPage)
+  data.value.slice(
+    currentPage.value * pageSize,
+    (currentPage.value + 1) * pageSize
+  )
 )
 
 const emit = defineEmits(['changeSearchValue'])
@@ -36,10 +79,18 @@ const emit = defineEmits(['changeSearchValue'])
 const changeSearchValue = (value) => {
   emit('changeSearchValue', value)
 }
+
+const currentPageChange = (value) => {
+  currentPage.value = value - 1
+}
 </script>
 
 <style lang="scss" scoped>
 .unit {
   width: 100%;
+
+  &__table {
+    margin-top: 40px;
+  }
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { fileURLToPath, URL } from 'url'
 
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import importElementPlus from 'vite-plugin-element-plus'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ElementPlus(),
+    importElementPlus(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
